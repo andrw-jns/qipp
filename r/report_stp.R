f10bc3b2546e028cc553b7ac320cbf1e6adbff52
Andrew Jones <Andrew.Jones@clients.its.local>
2017-11-01 16:39:36 +0000

    Added to report all flextables (incl. those with conditional formatting).

--- r/report_stp.R
+++ r/report_stp.R
@@ -19,19 +19,32 @@ qipp_report_stp <- addSlide(qipp_report_stp, "title" ) %>%
 
 # IP -----------------------------------------------------------------
 
+qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
+  addTitle("IP Spend 2016-17") %>%
+  addFlexTable(flex_spend_ip)
 
 qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
-  addTitle("Potential savings by matching average performers*") %>%
+  addTitle("Potential savings by matching average performers") %>%
   addFlexTable(flex_av_ip)
 
+qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
+  addTitle("Potential savings by matching top quartile performers") %>%
+  addFlexTable(flex_top_ip)
 
 qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
-  addTitle("Potential savings by matching top quartile performers*") %>%
-  addFlexTable(flex_top_ip)
+  addTitle("Rates vs. comparator CCGs") %>%
+  addFlexTable(flex_rates_ip)
+
+qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
+  addTitle("Change in Rate vs. comparator CCGs") %>%
+  addFlexTable(flex_roc_ip)
 
 
 # AE -----------------------------------------------------------------
 
+qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
+  addTitle("ED Spend 2016-17") %>%
+  addFlexTable(flex_spend_ae)
 
 qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
   addTitle("Potential savings by matching average performers*") %>%
@@ -42,9 +55,20 @@ qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
   addTitle("Potential savings by matching top quartile performers*") %>%
   addFlexTable(flex_top_ae)
 
+qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
+  addTitle("Rates vs. comparator CCGs") %>%
+  addFlexTable(flex_rates_ae)
+
+qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
+  addTitle("Change in Rate vs. comparator CCGs") %>%
+  addFlexTable(flex_roc_ae)
 
 
 # OP -----------------------------------------------------------------
+
+qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
+  addTitle("OP Spend 2016-17") %>%
+  addFlexTable(flex_spend_op)
 
 qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
   addTitle("Potential savings by matching average performers*") %>%
@@ -54,6 +78,15 @@ qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
 qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
   addTitle("Potential savings by matching top quartile performers*") %>%
   addFlexTable(flex_top_op)
+
+
+qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
+  addTitle("Rates vs. comparator CCGs") %>%
+  addFlexTable(flex_rates_op)
+
+qipp_report_stp <- addSlide(qipp_report_stp, "contentA") %>%
+  addTitle("Change in Rate vs. comparator CCGs") %>%
+  addFlexTable(flex_roc_op)
 
 
 #  -------------------------------------------------------------------
