4592ed6f84a43eb67bbbfc672d2a2997d4fb4b90
PowerUser <PowerUser@0AD00498.clients.its.local>
2017-11-21 14:40:50 +0000

    Removed Wolves from ae ambulance roc funnel, and their contribution to 12/13 trend.

--- r/master.R
+++ r/master.R
@@ -747,6 +747,9 @@ aeRoCAll <- aeRoCAll %>% left_join(
   , by = c("CCGCode", "Strategy", "From"))%>% 
   unique.data.frame() # to remove duplicates from incl. base yr in roc_all
 
+# Remove wolves ambulance: (12/13 is problem)
+aeRoCAll <- aeRoCAll %>% filter(!(Strategy == "AmbNoInvNoTreat_v1" & CCGCode == "06A")) # & FYear == 201213
+
 
 aeRoCActive <- aeRoCAll %>% roc_active
 
@@ -755,6 +758,7 @@ aeRoC <- aeRoCAll %>%
 
 aeRoCCheck <- aeRoC %>% filter(!IsValid)
 aeRoC <- aeRoC %>% filter(IsValid) 
+
 
 aeRoCSummary <- aeRoC %>% roc_summary
 
@@ -791,7 +795,50 @@ opTrendActive <- opSmall %>% trend_active
 # ipTrendComparators <- zest_comp3
 
 # ipTrendComparators <- ipSmall %>% trend_comparators 
-aeTrendComparators <- aeSmall %>% trend_comparators 
+
+
+# aeTrendComparators with Wolves 1213 ambulance removed
+aeTrendComparators <- aeSmall %>%
+  select(-DSCosts, -DSCostsVar, -Costs, -CCGDescription, -ShortName) %>%
+  gather(Strategy, Highlighted, -DSRate, -DSRateVar, -Spells, -CCGCode, -FYear, convert = T) %>%
+  filter(!(CCGCode == "06A" & Strategy == "AmbNoInvNoTreat_v1" & FYear == 201213)) %>% 
+  group_by(Strategy, FYear, CCGCode, Highlighted) %>%
+  summarise(
+    DSRate = sum(DSRate, na.rm = TRUE)
+    , DSRateVar = sum(DSRateVar, na.rm = TRUE)
+    , Spells = sum(Spells, na.rm = TRUE)
+  ) %>%
+  filter(Highlighted == 1) %>%
+  select(-Highlighted) %>%
+  mutate(
+    DerivedPopulation = (Spells / DSRate) * personYears
+    , SpellsCIUpper = (Spells + 1 ) * (1 - 1 / (9 * (Spells + 1)) + trendCV / (3 * sqrt(Spells + 1))) ^ 3
+    , SpellsCILower = Spells * (1 - 1 / (9 * Spells) - trendCV / (3 * sqrt(Spells))) ^ 3
+    , DSRateCIUpper = DSRate + sqrt(DSRateVar / Spells) * (SpellsCIUpper - Spells)
+    , DSRateCILower = DSRate + sqrt(DSRateVar / Spells) * (SpellsCILower - Spells)
+  ) %>%
+  group_by(Strategy, FYear, add = FALSE) %>%
+  summarise(
+    Average = sum(Spells, na.rm = TRUE) / sum(DerivedPopulation, na.rm = TRUE) * personYears
+    , TopQuartile = quantile(DSRate, 0.25, na.rm = TRUE)
+    , TopDecile = quantile(DSRate, 0.1, na.rm = TRUE)
+    , MaxDSRate = max(DSRate, na.rm = TRUE)
+    , MinDSRate = min(DSRate, na.rm = TRUE)
+  ) %>%
+  gather(Type, DSRate, -Strategy, -FYear, convert = TRUE) %>%
+  mutate(TypeNumber = ifelse(Type == "MinDSRate", 1,
+                             ifelse(Type == "TopDecile", 2, 
+                                    ifelse(Type =="TopQuartile", 3,
+                                           ifelse(Type == "Average", 4, 5))))) %>%
+  arrange(Strategy, FYear, TypeNumber) %>%
+  group_by(Strategy, FYear) %>%
+  mutate(Low = DSRate, High = lead(DSRate, 1)) %>%
+  filter(TypeNumber != 5)
+
+
+
+
+
 opTrendComparators <- opSmall %>% trend_comparators 
 
 # ipTrend adjustments for alcohol: (because ipSmall should really be changed):
